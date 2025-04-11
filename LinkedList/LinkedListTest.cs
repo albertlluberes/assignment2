6e7558785c9e50625aff48b57868d2931a35611a
Albert Lluberes Pineda <albert@VSUs-MacBook-Air.local>
2025-04-10 23:10:50 -0400

    Added test that inserts element at specified index

--- LinkedList/LinkedListTest.cs
+++ LinkedList/LinkedListTest.cs
@@ -6,3 +6,15 @@ public void Insert_AppendsElementToTail()
     list.Insert("second");
     Assert.Equal("second", list.Get(1));
 }
+[Fact]
+public void InsertAtIndex_InsertsElementAtCorrectPosition()
+{
+    var list = new LinkedList<string>();
+    list.Insert("a");
+    list.Insert("b");
+    list.Insert("d");
+
+    list.InsertAtIndex(2, "c");
+
+    Assert.Equal("c", list.Get(2));
+}
